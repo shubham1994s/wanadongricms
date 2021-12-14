7876d84bec85c40f45fb8d513194b222533cf423
rohittembhurne2194 <rohit.webeveloper.bigvtelecom@gmail.com>
2021-12-14 10:21:11 +0530

    Done By Rohit

---
+++ SwachhBharatAbhiyan.CMS/Areas/Solid/Controllers/SolidWasteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwachhBharatAbhiyan.CMS.Areas.Solid.Controllers
+{
+    public class SolidWasteController : Controller
+    {
+        // GET: Solid/SolidWaste
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
