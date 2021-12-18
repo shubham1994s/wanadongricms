4e95113ba2d80c6aeae1a48f2b4ccf2aefb30c8c
roshan1990s <r16smile@gmail.com>
2021-12-18 11:26:45 +0530

    Comit By Roshan

--- SwachhBharatAbhiyan.CMS/Areas/Liquid/LiquidAreaRegistration.cs
+++ SwachhBharatAbhiyan.CMS/Areas/Liquid/LiquidAreaRegistration.cs
@@ -18,7 +18,7 @@ namespace SwachhBharatAbhiyan.CMS.Areas.Liquid
                 "Liquid_default",
                 "Liquid/{controller}/{action}/{id}",
                 new {action = "Index", id = UrlParameter.Optional },
-                new { controller = "LiquidHome|LiquidWaste|LiquidEmployee|LiquidAttendence|LiquidGarbage|LiquidAndroid|LiquidMainMaster|LiquidAccount" },
+                new { controller = "LiquidHome|LiquidWaste|LiquidEmployee|LiquidAttendence|LiquidGarbage|LiquidAndroid|LiquidMainMaster|LiquidAccount|LiquidLocation" },
                new[] { "SwachhBharatAbhiyan.CMS.Areas.Liquid.Controllers" }
             );
         }
