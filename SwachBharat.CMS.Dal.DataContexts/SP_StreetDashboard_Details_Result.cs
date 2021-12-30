4ed796cd58ed7dd96fe5701d20e2c216d00d1245
roshan1990s <r16smile@gmail.com>
2021-12-30 13:33:16 +0530

    comit by roshan

--- SwachBharat.CMS.Dal.DataContexts/SP_StreetDashboard_Details_Result.cs
+++ SwachBharat.CMS.Dal.DataContexts/SP_StreetDashboard_Details_Result.cs
@@ -17,5 +17,6 @@ namespace SwachBharat.CMS.Dal.DataContexts
         public Nullable<int> TotalAttandence { get; set; }
         public Nullable<int> TotalHouse { get; set; }
         public Nullable<int> TotalStreet { get; set; }
+        public Nullable<int> TotalDump { get; set; }
     }
 }
