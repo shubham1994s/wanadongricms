f35d3bc2c0f74f199756365f18d5fba206fbb51f
milindwankhede <milindawankhede@gmail.com>
2022-01-05 18:50:52 +0530

    commit by milind

--- SwachhBharatAbhiyan.CMS/Areas/Liquid/Controllers/LiquidWasteController.cs
+++ SwachhBharatAbhiyan.CMS/Areas/Liquid/Controllers/LiquidWasteController.cs
@@ -44,7 +44,7 @@ namespace SwachhBharatAbhiyan.CMS.Areas.Liquid.Controllers
                 Redirect("/Account/Login");
         }
         // GET: Liquid/LiquidWaste
-        
+
         public ActionResult Index()
         {
             if (SessionHandler.Current.AppId != 0)
@@ -68,7 +68,7 @@ namespace SwachhBharatAbhiyan.CMS.Areas.Liquid.Controllers
         }
 
         public ActionResult AddLiquidWaste(int teamId = -1)
-         {
+        {
             if (SessionHandler.Current.AppId != 0)
             {
                 LiquidWasteVM dump = childRepository.GetLiquidWasteId(teamId);
@@ -124,7 +124,7 @@ namespace SwachhBharatAbhiyan.CMS.Areas.Liquid.Controllers
         }
 
 
-        public ActionResult Export(int id) 
+        public ActionResult Export(int id)
         {
             if (SessionHandler.Current.AppId != 0)
             {
@@ -151,48 +151,151 @@ namespace SwachhBharatAbhiyan.CMS.Areas.Liquid.Controllers
                 //string GridHtml = "<div style='width:100%;height: 100%;text-align: center;background: #fff;border : 2px solid black;'><div style='text-align:center;margin-top: 8px;font-size:22px;background: #abd037;'> O </div> <div style='background: #abd037;;font-weight: bold;font-size: 18px;'> " + AppDetails.AppName + "</div><div style='font-size: 15px;background: #abd037;'> House Id: " + details.ReferanceId + " </div><div style='height:10px;background: #abd037;'></div> <div style='height:10px;background: #fff;'></div><div style='background: #fff;'> <img style='width:250px;height:250px;' src='" + details.houseQRCode + "'/> </div></div>";
 
 
-               
-                    string src = AppDetails.baseImageUrlCMS + "/Content/images/icons/Nagpur_logo.png";
-                    //For Satana Only
-                   
-                    string top_img_new = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/Top_image.png";
-                    string slogan_new = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/slogan.png";
-                    string round = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/round.png";
 
-                    //string top_img_new = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/Top_image.png";
-                    //string slogan_new = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/slogan.png";
-                    //string round = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/round.png";
+                string src = AppDetails.baseImageUrlCMS + "/Content/images/icons/Nagpur_logo.png";
+                //For Satana Only
 
-                    GridHtml = "<div style='width:100%;height: 100%;background:#ffffff;border : 2px solid #4fa30a;'><div style='float:left;width:7%;padding-top:110px;padding-left:8px;'><img src='" + round + "' style = 'width:20px;height:20px;margin-left:5px;'/></div><div style='float:left;width:58%;padding-left:16px;padding-top:7px;'><img src='" + details.LWQRCode + "' style = 'width:20px;height:20px;'/></div><div style='float:left;width:83%;padding-left:5px;padding-top:10px;padding-bottom:6px;'><div style='padding-left:5px;'><img style='width:150px;height:95px;' src='" + top_img_new + "'/></div><div style='text-align: center;font-weight: 900;padding-bottom:3px;'>&nbsp;&nbsp;&nbsp;<span style='color:#000000;text-align: center;font-size: 16px'>LWC Id </span><br/><span style='color:#000000;text-align: center;font-size: 21px'>" + details.ReferanceId + "</span></div><div style='padding-left:5px;'><img src='" + slogan_new + "' style='width: 150px; height:49px;'/><br/></div></div><div style='float:left;width:3%;padding-top:110px;padding-left:22px;text-align:center;'><img src='" + round + "' style = 'width:20px;height:20px;'/></div></div>";
-         
+                string top_img_new = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/Top_image.png";
+                string slogan_new = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/slogan.png";
+                string round = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/round.png";
+
+                //string top_img_new = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/Top_image.png";
+                //string slogan_new = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/slogan.png";
+                //string round = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/round.png";
+
+                GridHtml = "<div style='width:100%;height: 100%;background:#ffffff;border : 2px solid #4fa30a;'><div style='float:left;width:7%;padding-top:110px;padding-left:8px;'><img src='" + round + "' style = 'width:20px;height:20px;margin-left:5px;'/></div><div style='float:left;width:58%;padding-left:16px;padding-top:7px;'><img src='" + details.LWQRCode + "' style = 'width:20px;height:20px;'/></div><div style='float:left;width:83%;padding-left:5px;padding-top:10px;padding-bottom:6px;'><div style='padding-left:5px;'><img style='width:150px;height:95px;' src='" + top_img_new + "'/></div><div style='text-align: center;font-weight: 900;padding-bottom:3px;'>&nbsp;&nbsp;&nbsp;<span style='color:#000000;text-align: center;font-size: 16px'>LWC Id </span><br/><span style='color:#000000;text-align: center;font-size: 21px'>" + details.ReferanceId + "</span></div><div style='padding-left:5px;'><img src='" + slogan_new + "' style='width: 150px; height:49px;'/><br/></div></div><div style='float:left;width:3%;padding-top:110px;padding-left:22px;text-align:center;'><img src='" + round + "' style = 'width:20px;height:20px;'/></div></div>";
+
 
 
                 using (MemoryStream stream = new System.IO.MemoryStream())
                 {
                     StringReader sr = new StringReader(GridHtml);
-                  
-                        var pgSize = new iTextSharp.text.Rectangle(324, 180);
-                        Document pdfDoc = new Document(pgSize, 1f, 1f, 1f, 1f);
-                        PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
-                        pdfDoc.Open();
-                        var content = writer.DirectContent;
-                        var pageBorderRect = new Rectangle(pdfDoc.PageSize);
 
-                        pageBorderRect.Left += pdfDoc.LeftMargin;
-                        pageBorderRect.Right -= pdfDoc.RightMargin;
-                        pageBorderRect.Top -= pdfDoc.TopMargin;
-                        pageBorderRect.Bottom += pdfDoc.BottomMargin;
+                    var pgSize = new iTextSharp.text.Rectangle(324, 180);
+                    Document pdfDoc = new Document(pgSize, 1f, 1f, 1f, 1f);
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+                    var content = writer.DirectContent;
+                    var pageBorderRect = new Rectangle(pdfDoc.PageSize);
 
-                        content.SetColorStroke(BaseColor.BLACK);
-                        content.SetLineWidth(5);
-                        content.Rectangle(pageBorderRect.Left, pageBorderRect.Bottom, pageBorderRect.Width, pageBorderRect.Height);
-                        content.Stroke();
-                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
-                        pdfDoc.Close();
-                    
+                    pageBorderRect.Left += pdfDoc.LeftMargin;
+                    pageBorderRect.Right -= pdfDoc.RightMargin;
+                    pageBorderRect.Top -= pdfDoc.TopMargin;
+                    pageBorderRect.Bottom += pdfDoc.BottomMargin;
+
+                    content.SetColorStroke(BaseColor.BLACK);
+                    content.SetLineWidth(5);
+                    content.Rectangle(pageBorderRect.Left, pageBorderRect.Bottom, pageBorderRect.Width, pageBorderRect.Height);
+                    content.Stroke();
+                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    pdfDoc.Close();
+
 
                     return File(stream.ToArray(), "application/pdf", Filename);
                 }
+            }
+            else
+                return Redirect("/Account/Login");
+        }
+
+
+        public ActionResult GenratePDF(string name, string ReferanceId)
+        {
+            if (SessionHandler.Current.AppId != 0)
+            {
+
+                var AppDetails = mainRepository.GetApplicationDetails(SessionHandler.Current.AppId);
+
+                string Filename = "";
+
+
+                string cdatetime = DateTime.Now.ToString("_ddmmyyyyhhmmss");
+
+                if (name != null && name != "")
+                {
+                    Filename = name + "(" + cdatetime + ").pdf";
+                }
+                else
+                {
+                    Filename = ReferanceId + "(" + cdatetime + ").pdf";
+                    name = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ";
+                }
+
+               
+
+                var guid = Guid.NewGuid().ToString().Split('-');
+                string image_Guid = DateTime.Now.ToString("MMddyyyymmss") + "_" + guid[1] + ".jpg";
+                var url = string.Format("http://api.qrserver.com/v1/create-qr-code/?data=" + ReferanceId);
+                WebResponse response = default(WebResponse);
+                Stream remoteStream = default(Stream);
+                StreamReader readStream = default(StreamReader);
+                WebRequest request = WebRequest.Create(url);
+                response = request.GetResponse();
+                remoteStream = response.GetResponseStream();
+                readStream = new StreamReader(remoteStream);
+
+
+                //Creating Path to save image in folder
+                System.Drawing.Image img = System.Drawing.Image.FromStream(remoteStream);
+                string imgpath = Path.Combine(Server.MapPath(AppDetails.basePath + AppDetails.LiquidQRCode), image_Guid);
+                var exists = System.IO.Directory.Exists(Server.MapPath(AppDetails.basePath + AppDetails.LiquidQRCode));
+                if (!exists)
+                {
+                    System.IO.Directory.CreateDirectory(Server.MapPath(AppDetails.basePath + AppDetails.LiquidQRCode));
+                }
+                img.Save(imgpath);
+                response.Close();
+                remoteStream.Close();
+                readStream.Close();
+
+
+                string GridHtml = string.Empty;
+                // string GridHtml = "<style>table{border-collapse: collapse;}table, td, th { padding:8px;border: 1px solid black;}<style><div><center><p style='margin-top:0px'><img style='width:200px;height200px' src='http://sbaappynitty.co.in:4022/Content/images/img/app_icon_cms.png'/></p><p style='font-size:25px;font-weight:bold;margin:0px'>Ghanta Gadi</p><p style='font-size:25px;font-weight:bold;margin:0px'>" + AppDetails.AppName + "</p><br/><hr/><br/></center></div><div><center><p style='font-size:32px;margin-bottom:15px'>< b id = 'hide_Name' > " + owner + "</b ></p ></center><table align='center' class='table'> <tr> <td style='font-weight: bold;'>House Number  </td> <td style=''>" + number + "</td> </tr> <tr> <td style='font-weight: bold;'>Ward Number  </td> <td style=''>" + ward.WardNo+ "</td> </tr> <tr> <td style='font-weight: bold;'>Area  </td> <td style=''> " + area.Name + "</td> </tr> <tr> <td style='font-weight: bold;'> House Id  </td> <td style=''>" + ReferanceId + "</td> </tr> </table><center><br/><p style='font-weight: bold;'> Scan Scaniffy Code <br /><br/>< img class='img-responsive' id='imggg' alt='hoto Not Found'  src='" +string.Format("https://api.qrserver.com/v1/create-qr-code/?data=" + ReferanceId ) + "'/></p><br /></center><div class='modal-footer'></div></div>";
+
+                //string slogan = AppDetails.baseImageUrlCMS + "/Content/images/icons/slogan.png";
+                //string topimg = AppDetails.baseImageUrlCMS + "/content/images/icons/top_outdoor.png";
+                //string leftbottomimg = AppDetails.baseImageUrlCMS + "/Content/images/icons/left_outdoor.png";
+                //string dry_wet_new = AppDetails.baseImageUrlCMS + "/Content/images/icons/Khapa/dry&wet.png";
+
+                //string src = AppDetails.baseImageUrlCMS + "/Content/images/icons/Nagpur_logo.png";
+
+                string top_img_new = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/Top_image.png";
+                string slogan_new = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/slogan.png";
+                string round = AppDetails.baseImageUrlCMS + AppDetails.basePath + "Content/icons/round.png";
+
+                //string top_img_new = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/Top_image.png";
+                //string slogan_new = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/slogan.png";
+                //string round = "http://localhost:34557/" + AppDetails.basePath + "Content/icons/round.png";
+
+
+                GridHtml = "<div style='width:100%;height: 100%;background:#ffffff;border : 2px solid #4fa30a;'><div style='float:left;width:7%;padding-top:110px;padding-left:8px;'><img src='" + round + "' style = 'width:20px;height:20px;margin-left:5px;'/></div><div style='float:left;width:58%;padding-left:16px;padding-top:7px;'><img src='" + imgpath + "' style = 'width:20px;height:20px;'/></div><div style='float:left;width:83%;padding-left:5px;padding-top:10px;padding-bottom:6px;'><div style='padding-left:5px;'><img style='width:150px;height:95px;' src='" + top_img_new + "'/></div><div style='text-align: center;font-weight: 900;padding-bottom:3px;'>&nbsp;&nbsp;&nbsp;<span style='color:#000000;text-align: center;font-size: 16px'>LWC Id</span><br/><span style='color:#000000;text-align: center;font-size: 21px'>" + ReferanceId + "</span></div><div style='padding-left:5px;'><img src='" + slogan_new + "' style='width: 150px; height:49px;'/><br/></div></div><div style='float:left;width:3%;padding-top:110px;padding-left:22px;text-align:center;'><img src='" + round + "' style = 'width:20px;height:20px;'/></div></div>";
+
+
+                //string GridHtml = "<div style='width:100%;height: 100%;text-align: center;background: #fff;border : 2px solid black;'><div style='text-align:center;margin-top: 8px;font-size:22px;background: #abd037;'> O </div> <div style='background: #abd037;;font-weight: bold;font-size: 18px;'> " + AppDetails.AppName + "</div><div style='font-size: 15px;background: #abd037;'> House Id: " + ReferanceId + " </div><div style='height:10px;background: #abd037;'></div> <div style='height:10px;background: #fff;'></div><div style='background: #fff;'> <img style='width:250px;height:250px;' src='" + string.Format("https://api.qrserver.com/v1/create-qr-code/?data=" + ReferanceId) + "'/>  </div></div>";
+                using (MemoryStream stream = new System.IO.MemoryStream())
+                {
+                    StringReader sr = new StringReader(GridHtml);
+                    var pgSize = new iTextSharp.text.Rectangle(324, 180);
+                    Document pdfDoc = new Document(pgSize, 1f, 1f, 1f, 1f);
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+                    var content = writer.DirectContent;
+                    var pageBorderRect = new Rectangle(pdfDoc.PageSize);
+
+                    pageBorderRect.Left += pdfDoc.LeftMargin;
+                    pageBorderRect.Right -= pdfDoc.RightMargin;
+                    pageBorderRect.Top -= pdfDoc.TopMargin;
+                    pageBorderRect.Bottom += pdfDoc.BottomMargin;
+
+                    content.SetColorStroke(BaseColor.BLACK);
+                    content.SetLineWidth(5);
+                    content.Rectangle(pageBorderRect.Left, pageBorderRect.Bottom, pageBorderRect.Width, pageBorderRect.Height);
+                    content.Stroke();
+                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    pdfDoc.Close();
+                    return File(stream.ToArray(), "application/pdf", Filename);
+                }
+
             }
             else
                 return Redirect("/Account/Login");
