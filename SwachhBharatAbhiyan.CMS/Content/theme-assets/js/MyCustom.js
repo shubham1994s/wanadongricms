2811665f08b677a612ad41109dbbf2748a7c98b3
shubham1994s <kawleshubham05@gmail.com>
2022-01-17 17:15:23 +0530

    comit by shubham

--- SwachhBharatAbhiyan.CMS/Content/theme-assets/js/MyCustom.js
+++ SwachhBharatAbhiyan.CMS/Content/theme-assets/js/MyCustom.js
@@ -1,13 +1,13 @@
-﻿debugger;
+﻿//debugger;
 window.onbeforeunload = function () {
-    debugger;
+    //debugger;
             var inputs = document.getElementsByTagName("button");
            /* var inputs = document.getelementsbytagname("button");*/
     /*var inputs = document.getElementsById("btnSubmit");*/
     for (var i = 0; i < inputs.length; i++) {
-        debugger;
+        //debugger;
         if (inputs[i].type == "button" || inputs[i].type == "submit") {
-            debugger;
+          //  debugger;
         inputs[i].disabled = true;
                 }
             }
