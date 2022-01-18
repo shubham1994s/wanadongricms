f5ea8e0df843e60dba1e1926b56b8a5e4247b025
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-01-18 11:06:59 +0530

    Comit By Rohit

--- SwachhBharatAbhiyan.CMS/Content/theme-assets/js/MyCustom.js
+++ SwachhBharatAbhiyan.CMS/Content/theme-assets/js/MyCustom.js
@@ -1,14 +1,22 @@
-﻿debugger;
-window.onbeforeunload = function () {
-    debugger;
-            var inputs = document.getElementsByTagName("button");
-           /* var inputs = document.getelementsbytagname("button");*/
-           /*var inputs = document.getElementsById("btnSubmit");*/
-    for (var i = 0; i < inputs.length; i++) {
-        debugger;
-        if (inputs[i].type == "button" || inputs[i].type == "submit") {
-            debugger;
-        inputs[i].disabled = true;
-                }
-            }
-        };
+﻿
+//window.onbeforeunload = function () {
+//    debugger;
+//    var inputs = document.getElementsById("btnSubmit");
+//           /* var inputs = document.getelementsbytagname("button");*/
+//           /*var inputs = document.getElementsById("btnSubmit");*/
+//    for (var i = 0; i < inputs.length; i++) {
+//        debugger;
+//        if (inputs[i].type == "button" || inputs[i].type == "submit") {
+//            debugger;
+//        inputs[i].disabled = true;
+//                }
+//            }
+//        };
+
+
+document.getElementById("btnSubmit").onclick = function () {
+    //disable
+    this.disabled = true;
+
+    //do some validation stuff
+}
