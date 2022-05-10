dd88d6e824d979d79c02100e9f26a814784811e8
shubham1994s <shubhamkawle41@gmail.com>
2022-05-09 18:14:11 -0700

    comit by shubham

--- SwachhBharatAbhiyan.CMS/DisplayReports.aspx.cs
+++ SwachhBharatAbhiyan.CMS/DisplayReports.aspx.cs
@@ -52,12 +52,22 @@ namespace SwachhBharatAbhiyan.CMS
                     //  string urlReportServer = "http://YOCC-2:82/reportServer";
                     //string urlReportServer = "http://192.168.100.123/ReportServer";
                     //string urlReportServer = "http://192.168.100.123/ReportServer";
-
-                    string urlReportServer = "http://202.65.157.253:85/ReportServer";
-                    //string urlReportServer = "http://COMP-7/ReportServer";
-                    //rptViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials("Administrator", "Pass@123", "192.168.100.7");
-                    rptViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials("Administrator", "Telec0m#B!9V", "202.65.157.253");
-
+                    string urlReportServer = "";
+                    int apid = Convert.ToInt32(AppID);
+                    //if(apid > 3102)
+                    //{ 
+                    // urlReportServer = "http://202.65.157.254:80/ReportServer";
+                    ////string urlReportServer = "http://COMP-7/ReportServer";
+                    ////rptViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials("Administrator", "Pass@123", "192.168.100.7");
+                    //rptViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials("Administrator", "Telec0m#B!9V", "202.65.157.254");
+                    //}
+                    //else
+                    //{
+                         urlReportServer = "http://202.65.157.253:85/ReportServer";
+                        //string urlReportServer = "http://COMP-7/ReportServer";
+                        //rptViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials("Administrator", "Pass@123", "192.168.100.7");
+                        rptViewer.ServerReport.ReportServerCredentials = new ReportServerCredentials("Administrator", "Telec0m#B!9V", "202.65.157.253");
+                  //  }
                     rptViewer.ProcessingMode = ProcessingMode.Remote;
 
                     rptViewer.ServerReport.ReportServerUrl = new Uri(urlReportServer);
