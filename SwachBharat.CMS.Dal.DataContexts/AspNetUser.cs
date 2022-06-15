cda51da6bf7b80c32d7479ee955a682573630ca5
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-06-15 15:08:27 +0530

    Comit By Rohit

--- SwachBharat.CMS.Dal.DataContexts/AspNetUser.cs
+++ SwachBharat.CMS.Dal.DataContexts/AspNetUser.cs
@@ -19,7 +19,6 @@ namespace SwachBharat.CMS.Dal.DataContexts
         {
             this.AspNetUserClaims = new HashSet<AspNetUserClaim>();
             this.AspNetUserLogins = new HashSet<AspNetUserLogin>();
-            this.AspNetRoles = new HashSet<AspNetRole>();
         }
     
         public string Id { get; set; }
@@ -35,12 +34,11 @@ namespace SwachBharat.CMS.Dal.DataContexts
         public int AccessFailedCount { get; set; }
         public string UserName { get; set; }
         public string Loginuser { get; set; }
+        public string PasswordString { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<AspNetRole> AspNetRoles { get; set; }
     }
 }
