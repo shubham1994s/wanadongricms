9b9da0cf1f77dcb7c8dbf19b9e434b70f216df29
milind wankhede <milindawankhede@gmail.com>
2022-07-15 16:03:29 +0530

    commit by Milind

--- SwachBharat.CMS.Dal.DataContexts/ChildSwachBharatAbhiyan.Context.cs
+++ SwachBharat.CMS.Dal.DataContexts/ChildSwachBharatAbhiyan.Context.cs
@@ -18,7 +18,7 @@ namespace SwachBharat.CMS.Dal.DataContexts
     public partial class DevChildSwachhBharatNagpurEntities : DbContext
     {
         public DevChildSwachhBharatNagpurEntities(int AppId)
-      : base(SwachBharatAppConnection.GetConnectionString(AppId))
+              : base(SwachBharatAppConnection.GetConnectionString(AppId))
         {
         }
 
@@ -65,6 +65,7 @@ namespace SwachBharat.CMS.Dal.DataContexts
         public virtual DbSet<GarbageCollectionDetail> GarbageCollectionDetails { get; set; }
         public virtual DbSet<SauchalayAddress> SauchalayAddresses { get; set; }
         public virtual DbSet<Daily_Attendance> Daily_Attendance { get; set; }
+        public virtual DbSet<EmpShift> EmpShifts { get; set; }
     
         public virtual ObjectResult<GetAttendenceDetailsTotal_Result> GetAttendenceDetailsTotal(Nullable<int> userId, Nullable<int> year, Nullable<int> month)
         {
